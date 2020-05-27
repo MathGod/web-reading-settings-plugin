76d14c88fc10a8079bee051d16a6cfe9c8c7e695
MathGod <hjsbswwc@gmail.com>
2020-05-27 22:23:21 +0800

    Create read.js

---
+++ read.js
@@ -0,0 +1,132 @@
+//阅读控件(字体大小，字体粗细，字体family，背景颜色，全屏缩屏)
+//字体大小
+          var oPtxt=document.getElementById("timu");
+          var oBtn1=document.getElementById("Btn1");
+          var oBtn2=document.getElementById("Btn2");
+if(!localStorage.zitidaxiao)
+{var num=22;
+document.getElementById("timu").style.fontSize=num+'px';}
+else
+{document.getElementById("timu").style.fontSize=localStorage.zitidaxiao;
+          var num=parseInt(localStorage.zitidaxiao);}
+          oBtn1.onclick = function(){
+              num++;
+              oPtxt.style.fontSize=num+'px';
+localStorage.zitidaxiao=document.getElementById("timu").style.fontSize;
+         };
+         oBtn2.onclick = function(){
+             num--;
+             oPtxt.style.fontSize=num+'px';
+localStorage.zitidaxiao=document.getElementById("timu").style.fontSize;
+         }
+
+//字体粗细
+if(localStorage.jiacu)
+document.getElementById("timu").style.fontWeight=localStorage.jiacu;
+function jiacu(){
+if(document.getElementById("timu").style.fontWeight!="611")
+{document.getElementById("timu").style.fontWeight="611";
+localStorage.jiacu="611";}
+else
+{document.getElementById("timu").style.fontWeight="normal";
+localStorage.jiacu="normal";}
+}
+
+//字体family，字体family按键自适应
+if(!localStorage.ziti)
+{localStorage.ziti="SimSun";
+localStorage.zitiname="宋体";
+document.getElementById("timu").style.fontFamily=localStorage.ziti;
+document.getElementById("ziti").innerHTML=localStorage.zitiname;}
+else
+{document.getElementById("timu").style.fontFamily=localStorage.ziti;
+document.getElementById("ziti").innerHTML=localStorage.zitiname;}
+function ziti(){
+if(localStorage.ziti=="SimSun")
+{localStorage.ziti="SimHei";
+localStorage.zitiname="黑体"}
+else if(localStorage.ziti=="SimHei")
+{localStorage.zitiname="楷体";
+localStorage.ziti="KaiTi";}
+else
+{localStorage.zitiname="宋体";
+localStorage.ziti="SimSun";}
+document.getElementById("timu").style.fontFamily=localStorage.ziti;
+document.getElementById("ziti").innerHTML=localStorage.zitiname;
+}
+    if(document.body.scrollWidth < 700){
+      document.getElementById('ziti').style.display = 'none';
+    }
+
+//背景颜色
+if (!localStorage.bg)
+{localStorage.bg="#18222d";
+localStorage.timu="#adadad";
+document.getElementById("bg").style.backgroundColor=localStorage.bg;
+document.getElementById("timu").style.color=localStorage.timu;}
+else
+{document.getElementById("bg").style.backgroundColor=localStorage.bg;
+document.getElementById("timu").style.color= localStorage.timu;}
+function bg(){
+if(localStorage.bg=="#ffffff")
+localStorage.bg="#f6f1e7";
+else if(localStorage.bg=="#f6f1e7")
+localStorage.bg="#dadada";
+else if(localStorage.bg=="#dadada")
+localStorage.bg="#dae9da";
+else if(localStorage.bg=="#dae9da")
+localStorage.bg="#ebcece";
+else if(localStorage.bg=="#ebcece")
+{localStorage.bg="#18222d";
+localStorage.timu= "#adadad";
+document.getElementById("timu").style.color= localStorage.timu;}
+else if(localStorage.bg=="#18222d")
+{localStorage.bg="#ebddae";
+localStorage.timu= "";
+document.getElementById("timu").style.color=localStorage.timu;}
+else if(localStorage.bg=="#ebddae")
+localStorage.bg="#dfecf0";
+else
+localStorage.bg="#ffffff";
+document.getElementById("bg").style.backgroundColor=localStorage.bg;
+}
+
+//全屏
+     function ExecuteFun(a) {
+        var full = a.innerHTML == '全屏';
+        full ? fullscreen() : exitfullscreen();
+        a.innerHTML = full ? '缩屏' : '全屏';
+        return false;
+    }
+
+function fullscreen() {
+  var el = document.documentElement,
+    rfs = el.requestFullScreen || el.webkitRequestFullScreen || el.mozRequestFullScreen || el.msRequestFullScreen,
+    wscript;
+  if(typeof rfs != "undefined" && rfs) {
+    rfs.call(el);
+    return;
+  }
+  if(typeof window.ActiveXObject != "undefined") {
+    wscript = new ActiveXObject("WScript.Shell");
+    if(wscript) {
+      wscript.SendKeys("{F11}");
+    }
+  }
+}
+
+function exitfullscreen() {
+  var el = document,
+    cfs = el.cancelFullScreen || el.webkitCancelFullScreen || el.mozCancelFullScreen || el.exitFullScreen,
+    wscript;
+  if (typeof cfs != "undefined" && cfs) {
+   cfs.call(el);
+   return;
+  }
+  if (typeof window.ActiveXObject != "undefined") {
+    wscript = new ActiveXObject("WScript.Shell");
+    if (wscript != null) {
+      wscript.SendKeys("{F11}");
+    }
+ }
+}
